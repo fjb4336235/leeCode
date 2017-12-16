2a5a3e6a02284ef3c84f21baed777bc374b7f146
531775012@qq.com <531775012@qq.com>
2017-12-16 16:43:53 +0800

    修改最长回文子串实现代码
    Change-Id: Ief7be28d2cfadf10228087b9258b0a3828465d7b
    Signed-off-by: 531775012@qq.com <531775012@qq.com>

--- leeCode/src/com/sxl/leecode/LongestPalindromicSubString.java
+++ leeCode/src/com/sxl/leecode/LongestPalindromicSubString.java
@@ -1,37 +1,57 @@
 package com.sxl.leecode;
 
+
 public class LongestPalindromicSubString {
 
-	public static String longestPalindrome(String s) {
+	/**
+	 * 采用Manacher‘s Algorithm算法计算最长回文子串
+	 * @param s
+	 * @return
+	 */
+	public String longestPalindrome(String s) {
         if(s == null || "".equals(s)){
         	return "";
         }else if(s.length() < 2){
         	return s;
         }else{
-        	int [][] pos = new int[127][2];
-        	int p = 0;
-        	while(p < s.length()){
-        		char c = s.charAt(p);
-        		if(pos[c][0] == 0){
-        			pos[c][0] = p + 1;
-        		}else{
-        			pos[c][1] = p + 1;
+        	char [] t = processString(s);
+        	int center = 0;
+        	int right = 0;
+        	int [] p = new int[t.length];
+        	for(int i = 1; i < t.length - 1; i++){
+        		if(right > i){
+        			p[i] = Math.min(right - i, p[2 * center - i]);
         		}
-        		p++;
-        	}
-        	int maxLen = 0;
-        	int maxPos = 0;
-        	for(int i = 0; i < 127; i++){
-        		int len = pos[i][1] - pos[i][0];
-        		if(len > maxLen){
-        			maxLen = len;
-        			maxPos = i;
+        		while(t[i - p[i] - 1] == t[i + p[i] + 1])
+        			p[i]++;
+        		if(i + p[i] > right){
+        			right = i + p[i];
+        			center = i;
         		}
         	}
-        	return s.substring(pos[maxPos][0] - 1, pos[maxPos][1]);
+        	int radix = 0;
+        	int c = 0;
+        	for(int i = 0; i < p.length; i++){
+        		if(p[i] > radix){
+        			radix = p[i];
+        			c = i;
+        		}
+        	}
+        	return s.substring((c - radix -1) / 2, (c -1 + radix) / 2);
         }
     }
+	private char [] processString(String s){
+		char [] chars = new char [s.length() * 2 + 3];
+		chars[0] = '$';
+		chars[s.length() * 2 + 2] = '@';
+		chars[s.length() * 2 + 1] = '#';
+		for(int i = 0; i < s.length(); i++){
+			chars[2 * i + 2] = s.charAt(i);
+			chars[i * 2 + 1] = '#';
+		}
+		return chars;
+	}
 	public static void main(String [] args){
-		System.out.println(longestPalindrome("abadd"));
+		System.out.println(new LongestPalindromicSubString().longestPalindrome("abadd"));
 	}
 }
